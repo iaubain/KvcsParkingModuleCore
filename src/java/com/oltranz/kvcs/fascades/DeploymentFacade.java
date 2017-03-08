42235e1cb3e982969d2b2dc82acb9ad603cf6066
iaubain <iaubain@yahoo.fr>
2017-03-08 09:44:20 +0200

    Modification in deployment facade to get  the last conductor deployment

--- src/java/com/oltranz/kvcs/fascades/DeploymentFacade.java
+++ src/java/com/oltranz/kvcs/fascades/DeploymentFacade.java
@@ -135,7 +135,7 @@ public class DeploymentFacade extends AbstractFacade<Deployment> {
         try{
             if(deployeeId.isEmpty())
                 return null;
-            Query q= em.createQuery("Select D from Deployment D WHERE D.contractId = :contractId AND D.deployeeId = :deployeeId ORDER BY D.id ASC");
+            Query q= em.createQuery("Select D from Deployment D WHERE D.contractId = :contractId AND D.deployeeId = :deployeeId ORDER BY D.id DESC");
             q.setParameter("deployeeId", deployeeId)
                     .setParameter("contractId", contractId)
                     .setMaxResults(1);
